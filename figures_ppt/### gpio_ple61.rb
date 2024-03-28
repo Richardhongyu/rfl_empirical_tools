029d5b3aa0e870036cb4cfb755b79db4be543cd9
Li Hongyu <lihongyu1999@bupt.edu.cn>
2024-03-28 11:46:51 +0800

    update

---
+++ figures_ppt/### gpio_ple61.rb
@@ -0,0 +1,43 @@
+### gpio_ple61
+probe
+- 1200/396
+- 668
+mask/unmask
+- 256/148
+get direction
+- 372/182
+irq type
+- 798/496
+resume
+- 628/392
+suspend
+- 368/320
+set wake
+- 324/56
+ack
+- 136+106/92
+
+### semaphore
+
+open
+- 244/184
+write
+- 284/121
+- 272
+read
+- 248/268
+- 228
+ioct!(read write)
+- 92+96+216+104/324
+init
+- 176/208
+- 900
+exit
+- 120/148
+- 16, 116
+release
+- 92/149
+consume
+- 206/248
+ops
+- 84/272
