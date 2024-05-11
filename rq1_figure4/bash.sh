0eaaca965c7b625cd6b699c22d9c5ae88aa30e36
Hongyu Li <543306408@qq.com>
2024-05-11 08:02:42 +0000

    update figures

--- rq1_figure4/bash.sh
+++ rq1_figure4/bash.sh
@@ -1,6 +1,7 @@
 cp code/*.py /workspace/rust-for-linux2/
 pushd /workspace/rust-for-linux2
-python3 split.py
+# This is a scrawling script. Commented for saving time. But it's fine to open it. This scrawl process will take a long time, please be patient.
+# python3 split.py
 python3 even_class.py > mid_value1
 python3 generate_data.py > mid_value2
 python3 percentage.py > mid_value3
