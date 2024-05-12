63543ba284224f86e5e9d5aff16bf79b33e64f0a
Yexuan Yang <1182282462@qq.com>
2024-05-12 23:48:03 +0800

    update draw.sh comments in figure7

--- rq2_figure7/draw.sh
+++ rq2_figure7/draw.sh
@@ -5,9 +5,7 @@ python3 only_left_subplot.py
 
 # We use `objdump` to see different sections in object files
 # If you want to collect these data, you should first compile rust-for-linux in rust-dev branch. defconfig is enough.
-# Then after the build, you should refer to the page at path [`All development-process docs` -> `Maintainer information` -> `List of maintainers`] in `docs.kernel.org`
-# For example, you can search the `io_uring` at this page, you will see a block titled with `io_uring` and files in `Files` field are files subsystem cared.
-# Just use `nm` or `llvm-nm` over the object files produced by these files, which tools to use depending on which compiler you use to build linux. 
+# Just use `objdump` over the object files produced by files in subsystems, which tools to use depending on which compiler you use to build linux. 
 # 
 # example:
 # objdump -h samples/rust/rust_semaphore.o
