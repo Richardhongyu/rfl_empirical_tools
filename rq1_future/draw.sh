9890743cfe67c6c1ccb290bcb3dd7e6ee140dad3
Yexuan Yang <1182282462@qq.com>
2024-05-12 11:03:30 +0800

    Add README.md in rq1_future

---
+++ rq1_future/draw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# These commands generate the file `rflcommits` `rfldevcommits` `commits`
+# We had already produced these file, If you want to reproduce, uncomment the lines 6 ~ 16
+# 
+# apt-get install -y git
+# cd <your-rust-for-linux-tree>
+# git fetch
+# git checkout rust
+# git log --oneline > data/rflcommits
+# git checkout rust-dev
+# git log --online > data/rfldevcommits
+# cd <your-linux-mainline-tree>
+# git fetch
+# git checkout master
+# git log --oneline > data/commits
+# 
+#
+cd code
+# The script `github.py` takes a long time to run. `github.py` generate the `data.txt` in `data` directory.
+# If you want to reproduce the data, uncomment it and run draw.sh
+#
+# python3 github.py > ../data/github.log
+
+# `draw.py` use the `data.txt` stored in `data` directory.
+# We had already produced `data.txt`, If you want to reproduce, uncomment the line 22
+python3 draw.py
